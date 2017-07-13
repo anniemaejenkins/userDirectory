b49039dc0c2f2a158bade50aae16e8e8d079f707
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-07-12 21:37:21 -0400

    fixed robot list, robot detail not showing

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.render('list', data);
+  // console.log(data);
 });
 
+app.get('/:id', (req, res) => {
+  res.render('robot', data);
+  // console.log("hello");
+});
 
 app.listen(3000);
