bf75ffd6be3457522096f4a2d498a2eeafd55c90
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-07-13 16:57:13 -0400

    added the rest of the info

--- app.js
+++ app.js
@@ -3,19 +3,26 @@ const express = require('express');
 const app = express();
 const mustacheExpress = require('mustache-express');
 
+app.use(express.static('public'));
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
-app.get('/', (req, res) => {
+app.get('/index', (req, res) => {
   res.render('list', data);
   // console.log(data);
 });
 
-app.get('/:id', (req, res) => {
-  res.render('robot', data);
-  // console.log("hello");
+app.get('/index/:id', (req, res) => {
+    var users;
+  for (var i = 0; i < data.users.length; i++) {
+    if(data.users[i].id == req.params.id){
+      users = data.users[i];
+    }
+  } console.log(users);
+  res.render('robot', users);
+  console.log("hello");
 });
 
 app.listen(process.env.PORT || 3000);
